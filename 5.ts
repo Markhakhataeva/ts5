be232ee446d0fdb16b91077fcaaa23d102473c7c
mike <mike@hk>
2024-04-19 22:56:35 +0300

    first com

---
+++ 5.ts
@@ -0,0 +1,49 @@
+let student1:{name:string;age:number;}
+student1={
+    name:"john",
+    age:22
+}
+
+
+////////
+
+
+interface Animal{
+    name:string
+    age:number
+}
+let dog:Animal={
+    name:"gabi",
+    age:1
+}
+
+/////
+
+interface Employee{
+    name:string
+    age:number
+    department?:string
+}
+
+let employee:Employee={
+    name:"jjj",
+    age:55
+}
+
+
+/////
+
+interface Student{
+    name:string
+    grades:number[]
+}
+
+let student:Student={
+    name:"kkk",
+    grades:[667]
+}
+
+////
+
+
+
